ea9f652c90de56caa0cd2fb139714bec77130ae6
Tung Nguyen <nvntung@gmail.com>
2025-07-06 13:15:18 +0100

    Test about sequental and parallel

---
+++ src/main/java/com/itersdesktop/javatechs/concurrent/ParallelForkJoinExample.java
@@ -0,0 +1,35 @@
+// https://dev.to/hugaomarques/paralelismo-e-concorrencia-102-java-parallel-streams-na-pratica-21g4
+package com.itersdesktop.javatechs.concurrent;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+public class ParallelForkJoinExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int availProcessors = Runtime.getRuntime().availableProcessors();
+        int maxNbThread = availProcessors > 4 ? (int) availProcessors/2 : 4;
+        ForkJoinPool customForkJoinPool = new ForkJoinPool(maxNbThread);
+
+        try {
+            customForkJoinPool.submit(() ->
+                numbers
+                    .parallelStream()
+                    .forEach(n -> { 
+                        operate(n);
+                    })
+            ).get();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            customForkJoinPool.shutdown();
+        }
+        
+    }
+
+    private static void operate(final int n) {
+        final String msg = "Thread: " + Thread.currentThread().getName() + " - Number: " + n;
+        System.out.println(msg);
+    }
+}
---
+++ src/main/java/com/itersdesktop/javatechs/concurrent/SequentalExample.java
@@ -0,0 +1,18 @@
+package com.itersdesktop.javatechs.concurrent;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SequentalExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        numbers
+            .stream()
+            .forEach(n -> System.out.println(operate(n)));
+    }
+
+    private static String operate(final int n) {
+        return "Thread: " + Thread.currentThread().getName() + " - Number: " + n;
+    }
+}
