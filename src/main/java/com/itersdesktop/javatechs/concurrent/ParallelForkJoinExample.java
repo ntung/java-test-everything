ebba43d2efd2b592c2edeac338b8e8a7579bd251
Tung Nguyen <nvntung@gmail.com>
2025-07-16 10:18:36 +0100

    Improve codes

--- src/main/java/com/itersdesktop/javatechs/concurrent/ParallelForkJoinExample.java
+++ src/main/java/com/itersdesktop/javatechs/concurrent/ParallelForkJoinExample.java
@@ -9,7 +9,7 @@ public class ParallelForkJoinExample {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         int availProcessors = Runtime.getRuntime().availableProcessors();
-        int maxNbThread = availProcessors > 4 ? (int) availProcessors/2 : 4;
+        int maxNbThread = availProcessors > 4 ? availProcessors /2 : 4;
         ForkJoinPool customForkJoinPool = new ForkJoinPool(maxNbThread);
 
         try {
--- src/main/java/com/itersdesktop/javatechs/concurrent/SequentialExample.java
+++ src/main/java/com/itersdesktop/javatechs/concurrent/SequentialExample.java
@@ -3,7 +3,7 @@ package com.itersdesktop.javatechs.concurrent;
 import java.util.Arrays;
 import java.util.List;
 
-public class SequentalExample {
+public class SequentialExample {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
